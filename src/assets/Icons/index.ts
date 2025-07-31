26f8b53c631c7ee034795d57f0baf7be93b1b985
tldms0507 <sieun050783@gmail.com>
2025-07-31 18:36:33 +0900

    DESIGN : 면접 기본 설정 아이콘, gap 수정

--- src/assets/Icons/index.ts
+++ src/assets/Icons/index.ts
@@ -27,3 +27,4 @@ export { default as Check } from "./tabler_check.svg?react";
 export { default as DownLoad } from "./tabler_download.svg?react";
 export { default as DownLoad2 } from "./tabler_download2.svg?react";
 export { default as Type } from "./type.svg?react";
+export { default as Map } from "./map_Pin.svg?react";
